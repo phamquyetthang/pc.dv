7e646eecc2e21a59b87de91ab66639d6a6cd46f0
phamquyetthang <17021033@vnu.edu.vn>
2020-08-01 09:20:01 +0700

    trang lien he, sửa đổi responsive

--- View/footer.php
+++ View/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
-    <div class="grid_footer">
-      <div>
+    <div class="flex_footer">
+      <div class="footer_item">
         <h2>CÔNG TY Cổ Phần Xây Dựng Và Thương Mại T & T</h2>
         Trụ Sở Chính : Thị Tứ Tân Thịnh - Yên Thắng - Ý Yên - Nam Định <br>
         Địa chỉ VP: Số 6 Ngõ 4 Mễ Trì Thượng - Đại Lộ Thăng Long - Nam Từ Liêm - Hà Nội.
@@ -13,8 +13,10 @@
         ☎️☎️☎️Hotline : <a href="tel: 0981202328">098 120 2328</a> Hoặc <a href="tel: 0981202328">0917605168</a><br>
         Email : nguyenthanhnd0812@gmail.com
       </div>
-      <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1975.8681717635718!2d105.78203579147257!3d21.0366361911331!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b55b011e49%3A0x9406c12dc4604160!2zxJDhuqFpIGjhu41jIFF14buRYyBnaWEgSMOgIE7hu5lpIChWTlUp!5e0!3m2!1svi!2s!4v1596268839964!5m2!1svi!2s" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <div class="footer_item">
+        <div class="myminimap">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1975.8681717635718!2d105.78203579147257!3d21.0366361911331!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454b55b011e49%3A0x9406c12dc4604160!2zxJDhuqFpIGjhu41jIFF14buRYyBnaWEgSMOgIE7hu5lpIChWTlUp!5e0!3m2!1svi!2s!4v1596268839964!5m2!1svi!2s" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
       </div>
     </div>
   </div>
